9b22c54e026955004d6b51945e4661aab0706618
Raunak shahu <raunakshahu.cg@gmail.com>
2026-03-29 12:18:44 +0530

    Backend ready for project p1

---
+++ server3/Backend/src/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const multer = require("multer");
+const uploadFile = require("./services/storage.service")
+const postModel = require("./models/post.model");
+
+const app = express();
+app.use(express.json());
+
+const Upload = multer({storage: multer.memoryStorage()})
+
+app.post("/create-post", Upload.single("image"), async(req,res)=>{
+    console.log(req.body)
+    console.log(req.file)
+
+    const result = await uploadFile(req.file.buffer);
+
+    const post = await postModel.create({
+        image : result.url,
+        caption : req.body.caption
+    })
+    return res.status(201).json({
+        mes: "post created successfully",
+        post
+    })
+    console.log(result)
+})
+
+app.get("/posts",async (req,res)=>{
+    const posts = await postModel.find()
+
+    return res.status(200).json({
+        mes: "posts fetched successfully",
+        posts : posts
+    })
+})
+
+module.exports = app;
