f6bfa38ee7f01c5e858a53bc7c1824fe0ec2292a
Raunak shahu <raunakshahu.cg@gmail.com>
2026-03-30 16:12:03 +0530

    Fronend

--- server3/Frontennd/src/pages/Feed.jsx
+++ server3/Frontennd/src/pages/Feed.jsx
@@ -1,17 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Feed.css'
+import axios from 'axios'
 
 function Feed() {
-    const [posts] = useState([
+    const [posts, setPosts] = useState([
         {
             _id: '1',
             image: 'https://ik.imagekit.io/1v8zhunyp/image_BR4u8_LAI',
             caption: 'A beautiful sunset at the beach.',
-        },
-       
-        
-        
+        }, 
     ])
+
+    useEffect(()=>{
+        axios.get('http://localhost:3000/posts')
+        .then((res)=>{
+            console.log(res)
+            setPosts(res.data.posts)
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+    },[])
   return (
     <section className={`feed-section ${posts.length === 1 ? 'single-post' : ''}`}>
         {posts.map((post, index) => (
