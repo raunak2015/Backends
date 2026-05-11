8d7b27f453f4a310892485b3cb1ae8b4e898d586
Raunak shahu <raunakshahu.cg@gmail.com>
2026-05-11 12:00:00 +0530

    Authentication practice

--- server4/src/app.js
+++ server4/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
-
+const authRoutes = require("./routes/auth.route");
 const app = express();
+app.use(express.json());
 
-
+app.use("/api/auth",authRoutes);
 module.exports = app;
